659e097691b3331f93b67567fea93fa1eafb1eb8
javieer24 <javiermonjesnuevo@gmail.com>
2023-12-10 22:57:02 -0600

    Se termina de crear listacircular

--- objects/ListaCircular.py
+++ objects/ListaCircular.py
@@ -21,3 +21,43 @@ class ListaCircular:
             self.head.anterior = self.cola
             self.cola.siguiente = self.head
         self.length += 1
+        
+    def getById(self, id):
+        if id < 0 or id >= self.length or self.head == None:
+            return None
+        else:
+            actual = self.head
+            while actual.id != id:
+                actual = actual.siguiente
+            return actual.value
+    def contains(self,object):
+        if self.head == None:
+            return None
+        else:
+            actual = self.head
+            while actual.siguiente != None:
+                if actual == object:
+                    break
+                else:
+                    actual = actual.siguiente
+            if actual != None:
+                return actual.siguiente
+            else:
+                return None
+            
+    def __str__(self):
+        string = "["
+        if self.head != None:
+            actual = self.head
+            while True:
+                if actual.siguiente == self.head:
+                    string += "{}]".format(actual)
+                else:
+                    string += "{},".format(actual)
+                actual = actual.siguiente
+                if actual == self.head:
+                    break
+                
+        else:
+            string += "]"
+        return string
